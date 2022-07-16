13abacf846a8263e6a88b81a98c4cf0af7972e4b
pelinalturk <80326351+pelinalturk@users.noreply.github.com>
2022-07-17 01:13:40 +0300

    Initial Commit

---
+++ design-patterns/src/test/java/com/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
