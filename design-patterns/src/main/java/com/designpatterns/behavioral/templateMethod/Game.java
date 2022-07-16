32e8980e3cd9c3d386bf40125e83484ac8b546f5
pelinalturk <80326351+pelinalturk@users.noreply.github.com>
2022-07-17 01:25:27 +0300

    Template Method Design Pattern

---
+++ design-patterns/src/main/java/com/designpatterns/behavioral/templateMethod/Game.java
@@ -0,0 +1,13 @@
+package com.designpatterns.behavioral.templateMethod;
+
+abstract class Game {
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+
+    public final void play(){
+        initialize();
+        startPlay();
+        endPlay();
+    }
+}
---
+++ design-patterns/src/main/java/com/designpatterns/behavioral/templateMethod/TemplateMethodDemo.java
@@ -0,0 +1,11 @@
+package com.designpatterns.behavioral.templateMethod;
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
